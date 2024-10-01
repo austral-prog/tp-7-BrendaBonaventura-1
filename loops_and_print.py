9fc5db5aab71311b27a3c45c7d1d1df58638c02a
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-10-01 15:33:38 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,11 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(listas):
+	li=[]
+	for index, lista in enumerate(listas):
+		li.append(f"{index}. {lista}")
+	return li
 
-
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(listas):
+	li=[]
+	for index, lista in enumerate(listas):
+		li.append(f"{index}. {lista[::-1]}")
+	return li
