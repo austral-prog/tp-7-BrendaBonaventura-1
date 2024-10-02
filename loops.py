bda92eb23908a6cd869680168c6049a1ff8f50ba
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-10-02 14:03:43 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,35 @@
-def index_of_by_index(word, list, index):
-    return -1
+def index_of_by_index(specific, lista, ind):
+	lista=lista[ind:]
+	for indice, colores in enumerate(lista):
+		if colores==specific:
+			return indice
+		else:
+			return -1
 
+def index_of_empty(lista):
+	for indice, words in enumerate(lista):
+		if words=="":
+			return indice
+		else:
+			return -1
 
-def index_of_empty(list):
-    return -1
+def index_of(specific, lista):
+	for indice, colores in enumerate(lista):
+		if colores==specific:
+			return indice
+		else:
+			return -1
 
+def put(sti, lista):
+	for indice, words in enumerate(lista):
+		if words=="":
+			words.replace("", sti)
+			return indice
+		else:
+			return -1
 
-def index_of(word, list):
-    return -1
-
-
-def put(word, list):
-    return -1
-
-
-def remove(word, list):
-    return -1
+def remove(sti, lista):
+	for words in lista:
+		if sti==words:
+			words.replace(sti, "")
+			return lista.count(sti)
